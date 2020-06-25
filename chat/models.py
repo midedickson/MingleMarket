325f034236582aa29246565cfb8e6a75510858cd
Dickson Olumide Sosanya <sosanyaolumidedoubled@gmail.com>
2020-06-25 18:23:56 +0100

    Transferring to new System

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Message(models.Model):
     author = models.ForeignKey(
@@ -12,4 +18,4 @@ class Message(models.Model):
         return self.author.username
 
     def last_10_messages():
-        return Message.objects.order_by('timestamp').all()[:10]
+        return Message.objects.order_by('-timestamp').all()[:10]
