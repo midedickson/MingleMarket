2f542857cc10a01b0e5d40520e00635fef69a544
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-07-21 00:03:34 +0100

    Built

---
+++ newfrontend/build/precache-manifest.269a1cecfaddd1accc9674db5106a3ac.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70017d55c1cab8f20cade252a1d5410b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7e6ddcefa9cff0b1ee",
+    "url": "/static/css/2.04b28932.chunk.css"
+  },
+  {
+    "revision": "94c8b7fc2433ce627972",
+    "url": "/static/css/main.a9605507.chunk.css"
+  },
+  {
+    "revision": "7a7e6ddcefa9cff0b1ee",
+    "url": "/static/js/2.b449a6a8.chunk.js"
+  },
+  {
+    "revision": "52ee930132bbb3e3eb5aefe850c39551",
+    "url": "/static/js/2.b449a6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c8b7fc2433ce627972",
+    "url": "/static/js/main.91ee891f.chunk.js"
+  },
+  {
+    "revision": "0936cf841357704c18f7",
+    "url": "/static/js/runtime-main.6de77123.js"
+  },
+  {
+    "revision": "e2603b8fd5694172d943b1f93ad45893",
+    "url": "/static/media/Sunfire - Young Free And Single.e2603b8f.mp3"
+  },
+  {
+    "revision": "3b59a9268e9f902c4e5509c9c253dc19",
+    "url": "/static/media/background-video.3b59a926.mp4"
+  },
+  {
+    "revision": "65ca6f4f80db18c0e6b24c17a70fb7b1",
+    "url": "/static/media/balloon1.65ca6f4f.PNG"
+  },
+  {
+    "revision": "e298c2fda962a982f683a1ffb5e2c07d",
+    "url": "/static/media/balloon2.e298c2fd.PNG"
+  },
+  {
+    "revision": "3f494630af84698209ed2758c31a20fb",
+    "url": "/static/media/balloon3.3f494630.PNG"
+  },
+  {
+    "revision": "a5d622e052bbd3cbd2f68ca2a84a44c3",
+    "url": "/static/media/cake1.a5d622e0.PNG"
+  },
+  {
+    "revision": "0c0bb880075d89523f64786b2a31f785",
+    "url": "/static/media/cake2.0c0bb880.PNG"
+  },
+  {
+    "revision": "10c967a0c108f62e764a17b6846818d5",
+    "url": "/static/media/hat1.10c967a0.PNG"
+  },
+  {
+    "revision": "43b485f686e73ca6c91bdbd626d7f622",
+    "url": "/static/media/hat2.43b485f6.PNG"
+  },
+  {
+    "revision": "836efd5590aac36f26509ba0568aa4d3",
+    "url": "/static/media/hey_mingle.836efd55.png"
+  },
+  {
+    "revision": "aa267554a204c4e221cbf773f606756c",
+    "url": "/static/media/logo.aa267554.png"
+  }
+]);
