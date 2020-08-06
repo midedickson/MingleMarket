82fde8a0d117b0a3cf61ba3fc1f4a9714173e6fd
Dickson Olumide <dicksonolumide@localhost.localdomain>
2020-08-07 00:10:10 +0100

    fix: social auth

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from chat.views import FacebookLogin
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
